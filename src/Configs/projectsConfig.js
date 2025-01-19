ee3ec712fe8e7e4460c44d95e41ba9b76d69c7cd
Mostafa Sharaf <93380966+mostafamohamedaly@users.noreply.github.com>
2025-01-19 13:28:15 +0400

    dgsdhhd

--- src/Configs/projectsConfig.js
+++ src/Configs/projectsConfig.js
@@ -2,20 +2,20 @@ const projects = [
   {
     id: "1",
     title: "Venu.ai",
-    screenshot: "./portfolio/Projects/Venu/2023-05-04-1956-52.gif",
+    screenshot: "/portfolio/Projects/Venu/2023-05-04-1956-52.gif",
     description:
       "Venu.ai is a cutting-edge platform connecting venue owners with individuals seeking unique spaces for events, meetings, and more. Simplifying the rental process, Venu.ai ensures seamless communication and agreements between renters and owners. With a wide range of venues and a focus on convenience and transparency, Venu.ai makes finding and booking the perfect space effortless.",
     url: "https://venu.ai",
     media: [
-      { src: "./portfolio/Projects/Venu/2023-05-04-1955-06.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1955-26.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1956-10.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1956-35.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1956-52.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1958-18.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-1958-39.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/2023-05-04-2008-54.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Venu/aboutus.PNG", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1955-06.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1955-26.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1956-10.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1956-35.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1956-52.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1958-18.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-1958-39.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/2023-05-04-2008-54.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Venu/aboutus.PNG", width: 400, height: 300 },
     ],
   },
   {
@@ -25,10 +25,10 @@ const projects = [
     description:
       "Linked is an emerging startup based in the USA, dedicated to game development and empowering individuals to invest in cryptocurrencies. Though its website is yet to launch, Linked aims to provide innovative gaming experiences while offering educational resources for navigating the crypto world. Combining creativity and financial literacy, Linked strives to bridge the gap between entertainment and investment opportunities.",
     media: [
-      { src: "./portfolio/Projects/Linked/2023-05-04-2015-22.gif", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Linked/Home Page.png", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Linked/Home Page2.png", width: 400, height: 300 },
-      { src: "./portfolio/Projects/Linked/Home Page3.png", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Linked/2023-05-04-2015-22.gif", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Linked/Home Page.png", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Linked/Home Page2.png", width: 400, height: 300 },
+      { src: "/portfolio/Projects/Linked/Home Page3.png", width: 400, height: 300 },
     ],
   },
 ];
