09bb441466be30e80d87beee0e2c3ba7b5f5bf2b
Mostafa Sharaf <93380966+mostafamohamedaly@users.noreply.github.com>
2025-01-19 12:41:40 +0400

    reverse

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Cursor from "./Components/Cursor";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes/AppRoutes.jsx";
 
 function App() {  
